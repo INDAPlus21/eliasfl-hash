09a5cd61df2a2d05cc3207b40bfbb257a4201645
Elias <elias@floreteng.se>
2022-02-11 14:27:48 +0100

    CLI refactor, db actions

--- src/db.rs
+++ src/db.rs
@@ -1,39 +1,12 @@
-use std::error::Error;
-use std::fmt;
-use std::str::FromStr;
 use std::sync::{Arc, RwLock};
 
 use crate::hash::HashMap;
 
-#[derive(Debug, Clone)]
-pub struct ArgParseError(String);
-impl fmt::Display for ArgParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "unable to parse argument action: {}", self.0)
-    }
-}
-impl Error for ArgParseError {}
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Action {
     Get,
-    Set,
     Insert,
     Remove,
-}
-
-impl FromStr for Action {
-    type Err = ArgParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match &s.to_lowercase()[..] {
-            "get" => Ok(Action::Get),
-            "set" => Ok(Action::Set),
-            "insert" => Ok(Action::Insert),
-            "remove" => Ok(Action::Remove),
-            string => Err(ArgParseError(string.to_owned())),
-        }
-    }
 }
 
 pub struct Database<K, V> {
@@ -52,5 +25,17 @@ impl Database<u64, String> {
             .read()
             .ok()
             .and_then(|guard| guard.get(&key.into()).map(|val| val.clone()))
+    }
+    pub fn insert(&self, key: u64, val: String) -> Option<String> {
+        self.records
+            .write()
+            .ok()
+            .and_then(|mut guard| guard.insert(key, val).map(|val| val.clone()))
+    }
+    pub fn remove(&self, key: u64) -> Option<String> {
+        self.records
+            .write()
+            .ok()
+            .and_then(|mut guard| guard.remove(&key.into()).map(|val| val.clone()))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
 use clap::Parser;
 
-use eliasfl_hash::db;
+use eliasfl_hash::db::Database;
 
-#[derive(Debug, Clone, Copy, Parser)]
+#[derive(Debug, Clone, Parser)]
 #[clap(author, version, about)]
-struct Cli {
-    action: db::Action,
-    key: Option<u8>,
-    value: Option<u8>,
+enum Cli {
+    /// Get value by key
+    Get {
+        /// The key to get
+        key: u64,
+    },
+    /// Insert value with key
+    Insert {
+        /// The key to insert at
+        key: u64,
+        /// The value to insert
+        value: String,
+    },
+    /// Remove value with key
+    Remove {
+        /// The key to remove
+        key: u64,
+    },
 }
 
 fn main() {
     let args = Cli::parse();
-    println!("{:#?}", args);
+
+    let db = Database::new();
+
+    let result = match args {
+        Cli::Get { key } => db.get(key),
+        Cli::Insert { key, value } => db.insert(key, value),
+        Cli::Remove { key } => db.remove(key),
+    };
+    println!("{:#?}", result);
 }
